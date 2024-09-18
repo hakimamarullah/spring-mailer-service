3b5bb9c8f2889b96487dc820acb9ce3ed5b2018f
hakimamarullah <hakimamarullah@gmail.com>
2024-09-18 08:19:36 +0700

    initial commit

---
+++ src/main/java/com/starline/spring/mailer/SpringMailerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.starline.spring.mailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starline/spring/mailer/SpringMailerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starline.spring.mailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
